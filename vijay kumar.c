44d7d0b9432da01c7938c23bb4876d4b09a2425f
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-02-02 13:35:55 +0530

    Create vijay kumar.c

---
+++ vijay kumar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+inta,b,c;
+printf("positive testcase");
+scanf("%d",&a);
+printf("negative testcase");
+scanf("%d",&b);
+if(a<=1,b>=1,c=0);
+{
+printf("the number is positive");
+else
+prinff("the number is negative");
+printf("the number is zero");
+}
+return 0;
+}
