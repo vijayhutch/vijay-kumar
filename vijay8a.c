92730ccb54f2a5996d2fb51898702722d21a4b59
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-02-19 15:58:54 +0530

    Rename vijay89.c to vijay8a.c

---
+++ vijay8a.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a,sum;
+printf("enter the number");
+scanf("%d",&a);
+int a=n
+while(n!=0)
+{
+a=n%10;
+sum=a*a*a;
+}
+printf("enter the check the armstrong number");
+if(sum==1)
+{
+printf("the armstrong number is%d",a);
+}
+else
+{
+printf("the armstrong is not a given number is%d",a);
+}
+return 0;
+}
