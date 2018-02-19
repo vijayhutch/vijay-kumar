e1db3701ea23bc9ba7a917a3d78c4a431735cc7c
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-02-19 16:25:37 +0530

    Create vijay25.c

---
+++ vijay25.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,n,a,d,sum=0;
+printf("\n Enter the number of arithmetic progression");
+scanf("%d",&n);
+printf("\n Enter the values");
+scanf("%d%d",&a,&d);
+for(i=1;i<=n;i++)
+{
+sum=sum+(a*a*a));
+}
+  printf("\nResult is %d",sum);
+return 0;
+}
