90cbfa2458fe146a1e435e7a97f564b333472d1f
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-02-19 16:09:04 +0530

    Create vijay90.c

---
+++ vijay90.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,min,max,i;
+printf("enter the armstrong between the range");
+scanf("%d%d",&min,&max);
+for(i=max;i<=min;i++);
+int num=0;
+int sum=0;
+while(n!=0)
+{
+a=n%10;
+temp=sum+a*a*a;
+}
+printf("%d%d",i);
+return 0;
+}
