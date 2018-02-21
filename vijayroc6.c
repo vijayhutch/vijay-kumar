f8d56f62c8da4f275ab6da2bb1cb5cc7510a74b2
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-02-21 16:17:41 +0530

    Update and rename vijayroc5.c to vijayroc6.c

--- vijayroc6.c
+++ vijayroc6.c
@@ -8,10 +8,6 @@ scanf("%d",&n);
 printf("enter the elements");
 for(i=0;i<=n;i++);
 {
-scanf("%d",&a[i]);
-}
-for(i=0;i<=n;i++);
-{
 for(j=0;j<=n-1;j++);
 {
 if(a[i]>a[j])
@@ -22,6 +18,7 @@ a[j]=temp;
 }
 }
 printf("%d",&a[0]);
-getch();
+print("the minimum element is %d");
+  return 0;
 }
 }
