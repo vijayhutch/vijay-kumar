edc05b2727ff3ffe493387a4f82ed81f2a687b62
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-02-21 16:05:57 +0530

    Create vijayroc2.c

---
+++ vijayroc2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int a[10],i,j,max,min;
+printf("enter the number");\
+scanf("%d",&n);
+}
+printf("enter the elements");
+for(i=0;i<=n;i++);
+{
+scanf("%d",&a[i]);
+}
+for(j=0;j<=n;j++);
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+printf("%d",&a[0]);
+return 0;
+}
+}
