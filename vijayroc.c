0245ab1c293ea44f49d3a5053a307fada2d60764
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-02-21 15:49:54 +0530

    Create vijayroc.c

---
+++ vijayroc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[50],n,max,i;
+printf("enter the range");
+scanf("%d",&n);
+printf("enter the array values");
+for(i=0;i<=n;i++)
+{
+scanf("%d",&i);
+max=a[0];
+}
+for("j=o;j<=n;j++");
+{
+scanf("%d",&i);
+}
+printf("maximum element is %d");
+return 0;
+}
