ae854a773d6825c2f00e0e03674e992533a51536
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-02 13:49:52 +0530

    Create vij4.c

---
+++ vij4.c
@@ -0,0 +1,14 @@
+include<stdio.h>
+#include<conio.h>
+int main()
+{
+float hour,minute,c;
+clrscr();
+  printf("enter the two values"):
+  scanf("%f%f",&hour,&minute);
+  {
+    c=hour-minute;
+    printf("%d",c);
+    getcch();
+    return 0;
+  }
