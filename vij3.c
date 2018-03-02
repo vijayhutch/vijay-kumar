1c1beed3406de627686183a9db5f26d6ab526a05
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-02 13:40:59 +0530

    Create vij3.c

---
+++ vij3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int min,hr,c,n;
+scanf("%d",&a,&b);
+if(min>=60)
+{
+
+hour=min/60;
+min=min%60
+}
+printf("%d,%d",hour,min);
+getch();
+return 0;
+}
