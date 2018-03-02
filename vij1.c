91190e7c6d31e5bd279dd84bea19f9b5fc3c294b
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-02 13:15:02 +0530

    Create vij1.c

---
+++ vij1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a[10],b[20]i,a,n;
+printf("enter the limimt");
+scanf("%d",&a);
+printf("enter the element");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("the elements with index");
+for(i=0;i<n;i++)
+{
+printf("%d\t%d\n",i,a[i]);
+getch();
+}
