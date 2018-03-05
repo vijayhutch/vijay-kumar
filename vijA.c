39897e4dce759b7ef900f0aee40affadb85e75fc
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-05 15:52:21 +0530

    Update vijA.c

--- vijA.c
+++ vijA.c
@@ -2,8 +2,7 @@
 #include<string.h>
 void main()
 {
-char str[100];
-int i = 0, l = 0, f = 1;
+Char i = 0, l = 0, f = 1;
 puts("Enter any string\n");
 gets(str);
 for(i = 0;str[i]!='\0';i++)
@@ -11,3 +10,5 @@ for(i = 0;str[i]!='\0';i++)
 l = l + 1;
 }
 printf("The number of characters in the string are %d\n", l);
+  return 0;
+}
