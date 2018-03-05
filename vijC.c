2b19a5b760ccfb6b111ebca2cb0ec6c83e07710c
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-05 16:03:26 +0530

    Create vijC.c

---
+++ vijC.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char a[50];
+	int i,count=0;
+	gets(a);
+  }
+	
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i] =="0'"
+		count++;
+		
+		
+	}
+	printf("%d",count);
+	return 0;
+  }
