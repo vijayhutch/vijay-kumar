812b6a9de060d491cb888653365db6b90685813d
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-05 15:56:05 +0530

    Create vijB.c

---
+++ vijB.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+char s[100];
+int count=0,i;
+clrscr();
+printf("\n Enter the sentence:");
+scanf("%s",s);
+for(i=0;s[i]!=';i++)
+{
+if(s[i]= ')
+count++;    
+}
+printf("\n Number of words are: %d",count+1);
+getch();
+return 0;
+}
