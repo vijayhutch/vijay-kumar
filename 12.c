d69e5ce3deba5add6c982508b306bbd3e816f66c
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-07 15:56:56 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+char str[50],i,c=0;
+printf("enter the word");
+gets(str);
+for(i=0;str[i]!='\0';i++)
+}
+else
+{
+    c++;
+}
+}
+
+{
+printf("%d",c);
+return 0;
+}
