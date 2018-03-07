86ff62b20e5140747ca1871c67bf8a85c252cd80
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-07 15:52:26 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char[50],i,str[50],d;
+printf("enter the world");
+gets(strl)
+for(i=o,str[i],i++)
+{
+if(str i='0'&&str '1');
+{
+i++
+}
+}
+printf("%d",d);
+}
