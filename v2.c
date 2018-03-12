7b65a5364fbf5bd92e240de07b7a51697976570a
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-12 15:57:44 +0530

    Create v2.c

---
+++ v2.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+void main()
+{
+char str[5];
+  int k,i;
+printf("enter a string");
+scanf("%s",&str);
+printf("enter a limit");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+printf("%s",str);
+}
+printf("Enter the number:\n");
+scanf("%d\n",&n);
+while(i<=n)
+{
+printf("%s\n",a);	
+i++;	
+}
