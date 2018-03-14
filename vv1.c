7dfbba503d966a9c9ab7ed37e269fb9db6b46737
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-14 15:54:17 +0530

    Create vv1.c

---
+++ vv1.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char a[20];
+	char b[20];
+	int l,n,i;
+	int sum1=0;
+	int sum2=0;
+	scanf("%s",a);
+	scanf("%s",b);
+
+	 }
+	 if(sum1>sum2){
+	 	printf("%s",a);
+	 }
+	 else{
+	 	printf("%s",b);
+	 }
+}
