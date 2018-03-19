d8ec80cbc798cbeb4b6acdb422a6fa1b7c245fb9
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-19 03:18:02 -0700

    Create 124.c

---
+++ 124.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main(void) 
+{
+	int w1,w2,sum;
+	printf("\n enter the first number:");
+	scanf("%d",&w1);
+	printf("\n enter the second number:");
+	scanf("%d",&w2);
+	sum=w1+w2;
+	printf("%d",sum);
+	return 0;
+}
