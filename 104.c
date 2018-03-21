a1b396bcc57d84c4c702ae800e95582575b5d0c9
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-21 16:21:25 +0530

    Create 104.c

---
+++ 104.c
@@ -0,0 +1,14 @@
+#include<string.h>
+int main()
+{
+   int x,y,t;
+   scanf("%d %d",&x,&y);
+   if(a!=0)
+   {
+       t=x;
+       x=y;
+       y=t;
+   }
+   printf("%d %d",x,y);
+    return 0;
+}
