b1536c2b8ade8da3e60535392ab72ae8401bb4fe
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-21 16:22:40 +0530

    Update 104.c

--- 104.c
+++ 104.c
@@ -1,14 +1,14 @@
 #include<string.h>
 int main()
 {
-   int x,y,t;
-   scanf("%d %d",&x,&y);
+   int a,b,c;
+   scanf("%d %d",&a,&b);
    if(a!=0)
    {
-       t=x;
-       x=y;
-       y=t;
+       t=a;
+       x=b;
+       y=c;
    }
-   printf("%d %d",x,y);
+   printf("%d %d",a,b);
     return 0;
 }
