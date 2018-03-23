4eafdea648cbfcdff671609d57fcf447035a976b
vijayhutch <35995186+vijayhutch@users.noreply.github.com>
2018-03-23 16:02:08 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int a,i;
+	char str[10];
+	gets(str);
+	printf("enter a");
+	scanf("%d",&a);
+	for(i=0;i<n;i++)
+	{
+		printf("%c",str[i]);
+	}
+	
+}
